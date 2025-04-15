9a107d32ce543c01ecbf043ed1eb12bdd66154fe
MateiBiancaM <160779868+MateiBiancaM@users.noreply.github.com>
2025-04-15 22:17:30 +0300

    First commit

---
+++ src/components/CardComponent.jsx
@@ -0,0 +1,62 @@
+import { Box, Flex, Image, Text } from '@chakra-ui/react'
+import React from 'react'
+import { Link } from "react-router-dom";
+import { imagePath } from '../services/api';
+import { StarIcon } from '@chakra-ui/icons';
+
+const CardComponent = ({item, type}) => {
+  return (
+    <Link to={`/${type}/${item?.id}`}>
+        <Box 
+            position="relative" 
+            overflow="hidden" 
+            role="group" 
+            cursor="pointer"
+            transform="scale(1)"
+            transition="transform 0.3s ease-in-out"
+            _hover={{
+              transform: "scale(1.05)"
+            }}
+        >
+            <Image 
+                src={`${imagePath}/${item.poster_path}`} 
+                alt={item?.title || item?.name} 
+                width="100%"
+                height="100%"
+                objectFit="cover"
+            />
+            <Box 
+                position="absolute"
+                bottom="0"
+                left="0"
+                w="100%"
+                h="33%"
+                bg="rgba(0,0,0,0.8)"
+                color="white"
+                opacity="0"
+                p="2"
+                transition="opacity 0.3s ease-in-out"
+                _groupHover={{ opacity: 1 }}>
+                <Text textAlign={"center"}>
+                    {item?.title || item?.name}
+                </Text>
+                <Text textAlign={"center"} fontSize={"small"} color={"green.200"}>
+                    {new Date(item?.release_date || item?.first_air_date).getFullYear() || "N/A"}
+                </Text>
+                <Flex
+                    alignItems={"center"}
+                    justifyContent={"center"}
+                    gap={"2"}
+                    mt="4"
+                    >
+                    <StarIcon fontSize={"small"}/>
+                    <Text>{item?.vote_average?.toFixed(1)}</Text>
+                   
+                </Flex>
+            </Box>
+        </Box>
+    </Link>
+  )
+}
+
+export default CardComponent
---
+++ src/main.jsx
@@ -0,0 +1,78 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+import App from './App.jsx'
+import theme from '../theme.js'
+import Home from './pages/Home.jsx'
+import Movies from './pages/movies/Movies.jsx'
+import Shows from './pages/shows/Shows.jsx'
+import Search from './pages/search/Search.jsx'
+import DetailsPage from './pages/DetailsPage.jsx'
+import { AuthProvider } from './context/authProvider.jsx'
+import WatchList from './pages/WatchList.jsx'
+import Protected from './components/routes/Protected.jsx'
+
+localStorage.setItem('chakra-ui-color-mode', 'dark')
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/movies",
+        element: <Movies />,
+      },
+      {
+        path: "/shows",
+        element: <Shows />,
+      },
+      {
+        path: "/:type/:id",
+        element: <DetailsPage />,
+      },
+      {
+        path: "/wishlist",
+        element: <div>Wishlist</div>,
+      },
+      {
+        path: "/watchlist",
+        element: <Protected><WatchList/></Protected>,
+      },
+      {
+        path: "/favorites",
+        element: <div>Favorites</div>,
+      },
+      {
+        path: "/financial",
+        element: <div>Financial</div>,
+      },
+      {
+        path: "/consumption",
+        element: <div>Consumption</div>,
+      },
+      {
+        path: "/search",
+        element: <Search />,
+      },
+
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ColorModeScript initialColorMode="dark" />
+    <ChakraProvider theme={theme}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </ChakraProvider>
+  </StrictMode>,
+)
